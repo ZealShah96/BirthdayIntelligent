e6d8f55dfbb0ba259443ba48802cbcd662c9e928
zeal shah <zealshah96@gmail.com>
2018-07-31 22:47:21 +0530

    First Time Npm Install And Date used

--- findDates.js
+++ findDates.js
@@ -1,11 +1,11 @@
 var supportFnctions=require('./supportFunctions');
-
+let date = require('date-and-time');
 
 var dateCalculationFunctions={};
 
-var birthdayDate=Date.parse("09-05-1996","DD-MM-YYYY");
+var birthdayDate=date.parse("09-05-1996","DD-MM-YYYY");
 supportFnctions.logging(birthdayDate);
-//dateCalculationFunctions.
+//dateCalculationFunctions
 
 
 
